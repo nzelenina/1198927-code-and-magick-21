d31ba7a62893d2956d1576ac6769c6b520b09d73
nataliya <n.zelenina1967@gmail.com>
2020-09-21 17:37:25 +0300

    статистика цвет

--- js/stat.js
+++ js/stat.js
@@ -3,7 +3,7 @@ var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
-var TITLE_HEIGHT = 60;
+var TITLE_HEIGHT = 70;
 var GAP = 50;
 var FONT_GAP = 10;
 var TEXT_HEIGHT = 20;
@@ -34,16 +34,22 @@ window.renderStatistics = function (ctx,players,times) {
 
   ctx.font = '16px PT Mono';
   ctx.fillStyle = '#000';
-  ctx.fillText('Ура вы победили', 110, 20);
-  ctx.fillText('Список результатов', 110, 40);
+  ctx.fillText('Ура вы победили', CLOUD_X + GAP, 40);
+  ctx.fillText('Список результатов', CLOUD_X + GAP, 60);
 
  var maxTime = getMaxElement(times);
-
+ function getRandomSaturation () {
+  return Math.round(Math.random() * 100)
+}
 
   for (var i = 0; i < players.length; i++) {
   ctx.fillStyle = '#000';
   ctx.fillText(players[i],CLOUD_X + GAP + (BAR_WIDTH + GAP) * i, BAR_Y);
-  ctx.fillStyle = 'blue';
+  if (players[i] === 'Вы') {
+  ctx.fillStyle = 'rgba(255, 0, 0, 1)'
+} else {
+  ctx.fillStyle = 'hsl(240,'+ getRandomSaturation() + '%, 50%)'
+};
   ctx.fillRect(CLOUD_X + GAP + (BAR_WIDTH + GAP) * i, BAR_Y - TEXT_HEIGHT - FONT_GAP, BAR_WIDTH, (barHeight * times[i]) / maxTime);
   var round = Math.round(times[i]);
   ctx.fillStyle = '#000';
