8d421c1027735717507882ce74c67570cbdc3989
nataliya <n.zelenina1967@gmail.com>
2020-09-24 11:54:37 +0300

    орда волшебников

---
+++ js/setup.js
@@ -0,0 +1,52 @@
+'use strict';
+
+var firstNames = ['иван', 'хуан-себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var lastNames = ['да Марья', 'Верон', 'Мирабелла', 'Топольницкая', 'Нионго', 'Ирвинг', 'Онопко'];
+var coatsColors = ['rgb (101, 137, 164)', 'rgb (241, 43, 107)', 'rgb (146, 100, 161)', 'rgb (56, 159, 117)', 'rgb (215, 210, 55)', 'rgb (0, 0, 0)'];
+var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
+var wizards = [];
+for (var i = 0; i < 4; i++) {
+
+  var object = {
+    name: getRandomArrayElement(lastNames) + ' ' + getRandomArrayElement(firstNames),
+    coatsColor: getRandomArrayElement(coatsColors),
+    eyesColor: getRandomArrayElement(eyesColors)
+  };
+  wizards.push(object);
+};
+console.log(wizards);
+
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+function getRandomArrayElement(array) {
+  return array[getRndInteger(0, array.length - 1)];
+};
+
+var lastName = getRandomArrayElement(lastNames);
+var firstName = getRandomArrayElement(firstNames);
+var userDialog = document.querySelector('.setup');
+userDialog.classList.remove('hidden');
+
+document.querySelector('.setup-similar').classList.remove('hidden');
+var similarListElement = document.querySelector('.setup-similar-list');
+var similarWizardTemplate = document.querySelector('#similar-wizard-template')
+  .content;
+
+var renderWizard = function(wizard) {
+  var wizardElement = similarWizardTemplate.cloneNode(true);
+
+  wizardElement.querySelector('.setup-similar-label').textContent = wizards[i].name;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizards[i].coatsColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizards[i].eyesColor;
+
+  similarListElement.appendChild(wizardElement);
+
+  return wizardElement;
+}
+var fragment = document.createDocumentFragment();
+for (var i = 0; i < 4; i++) {
+  fragment.appendChild(renderWizard(wizards[i]));
+}
+similarListElement.appendChild(fragment);
